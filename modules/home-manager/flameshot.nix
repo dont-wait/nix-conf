34769ffb1e42b397644b713fe89746455c3dcbae
dont-wait <nguyentansang15042005@gmail.com>
2026-04-28 15:41:57 +0700

    use stable flameshot

--- modules/home-manager/flameshot.nix
+++ modules/home-manager/flameshot.nix
@@ -1,25 +1,14 @@
-# ~/.config/home-manager/flameshot.nix
-{ pkgs, ... }:
+{
+  inputs,
+  pkgs,
+  ...
+}:
 
+let
+  pkgs-stable = inputs.nixpkgs-stable.legacyPackages.${pkgs.system};
+in
 {
   home.packages = [
-    (pkgs.flameshot.overrideAttrs (oldAttrs: rec {
-      version = "12.1.0";
-      src = pkgs.fetchFromGitHub {
-        owner = "flameshot-org";
-        repo = "flameshot";
-        rev = "v${version}";
-        sha256 = "sha256-omyMN8d+g1uYsEw41KmpJCwOmVWLokEfbW19vIvG79w=";
-      };
-    }))
+    pkgs-stable.flameshot
   ];
-
-  services.flameshot = {
-    enable = true;
-    settings = {
-      General = {
-        showStartupLaunchMessage = false;
-      };
-    };
-  };
 }
