f34f7d2d502620638c1488d370e949673f04efe4
dont-wait <nguyentansang15042005@gmail.com>
2026-05-01 03:52:48 +0700

    fuk treesitter

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -1,35 +1,37 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = "main",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter").setup({
-            ensure_installed = {
-                "c",
-                "lua",
-                "vim",
-                "javascript",
-                "typescript",
-                "tsx",
-                "html",
-                "go",
-                "gomod",
-                "gowork",
-                "gosum",
-                "java",
-                "json",
-                "zig",
-                "c_sharp",
-                "dart"
-            },
-            highlight = { enable = true },
-        })
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter").setup({
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"javascript",
+				"typescript",
+				"tsx",
+				"html",
+				"go",
+				"gomod",
+				"gowork",
+				"gosum",
+				"java",
+				"json",
+				"zig",
+				"c_sharp",
+				"dart",
+			},
+			sync_install = false,
+			indent = { enable = true },
+			auto_install = true,
+			highlight = { enable = true },
+		})
 
-        -- Enable highlight tự động cho tất cả filetype
-        vim.api.nvim_create_autocmd("FileType", {
-            callback = function()
-                pcall(vim.treesitter.start)
-            end,
-        })
-    end,
+		-- -- Enable highlight tự động cho tất cả filetype
+		-- vim.api.nvim_create_autocmd("FileType", {
+		-- 	callback = function()
+		-- 		pcall(vim.treesitter.start)
+		-- 	end,
+		-- })
+	end,
 }
