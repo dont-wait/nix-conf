25fa56195793354272f04bf4744b6d9c794eca25
dont-wait <nguyentansang15042005@gmail.com>
2026-05-07 16:02:08 +0700

    config hurl keymap

--- dotfiles/nvim/lua/plugins/hurl.lua
+++ dotfiles/nvim/lua/plugins/hurl.lua
@@ -45,11 +45,11 @@ return {
     },
     keys = {
         -- Run API request
-        { "<leader>A",  "<cmd>HurlRunner<CR>",        desc = "Run All requests" },
-        { "<leader>a",  "<cmd>HurlRunnerAt<CR>",      desc = "Run Api request" },
-        { "<leader>te", "<cmd>HurlRunnerToEntry<CR>", desc = "Run Api request to entry" },
-        { "<leader>tE", "<cmd>HurlRunnerToEnd<CR>",   desc = "Run Api request from current entry to end" },
-        { "<leader>tm", "<cmd>HurlToggleMode<CR>",    desc = "Hurl Toggle Mode" },
+        { "<leader>ha",  "<cmd>HurlRunner<CR>",        desc = "Run All requests" },
+        { "<leader>hr",  "<cmd>HurlRunnerAt<CR>",      desc = "Run Api request" },
+        { "<leader>he", "<cmd>HurlRunnerToEntry<CR>", desc = "Run Api request to entry" },
+        { "<leader>hE", "<cmd>HurlRunnerToEnd<CR>",   desc = "Run Api request from current entry to end" },
+        { "<leader>ht", "<cmd>HurlToggleMode<CR>",    desc = "Hurl Toggle Mode" },
         { "<leader>tv", "<cmd>HurlVerbose<CR>",       desc = "Run Api in verbose mode" },
         { "<leader>tV", "<cmd>HurlVeryVerbose<CR>",   desc = "Run Api in very verbose mode" },
         -- Run Hurl request in visual mode
