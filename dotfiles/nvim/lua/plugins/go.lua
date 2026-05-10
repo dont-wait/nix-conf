97d0f836fa62cb95159b057abd0a60a49678a8b7
dont-wait <nguyentansang15042005@gmail.com>
2026-05-10 11:33:26 +0700

    refactor to avoid override treesitter with nix

--- dotfiles/nvim/lua/plugins/go.lua
+++ dotfiles/nvim/lua/plugins/go.lua
@@ -1,16 +1,22 @@
 return {
-	"ray-x/go.nvim",
-	dependencies = { -- optional packages
-		"ray-x/guihua.lua",
-		"neovim/nvim-lspconfig",
-		"nvim-treesitter/nvim-treesitter",
-	},
-	config = function()
-		require("go").setup({
-			lsp_cfg = false,
-		})
-	end,
-	event = { "CmdlineEnter" },
-	ft = { "go", "gomod" },
-	build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
+    "ray-x/go.nvim",
+    dependencies = {
+        "ray-x/guihua.lua",
+        "neovim/nvim-lspconfig",
+        "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+        require("go").setup({
+            lsp_cfg = false,
+        })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = { "go", "gomod" },
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
 }
