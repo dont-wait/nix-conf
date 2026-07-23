0c05410b2d33869b00c6cb0a8d47d513365168ae
dont-wait <nguyentansang15042005@gmail.com>
2026-07-23 10:46:21 +0700

    clipboard zathura

--- modules/home-manager/zathura.nix
+++ modules/home-manager/zathura.nix
@@ -9,9 +9,9 @@ in
     # zathura-djvu
     # zathura-ps
   ];
+  xdg.configFile."zathura/zathurarc".source = "${themeDir}/zathurarc";
   xdg.configFile."zathura/themes/catppuccin.theme".source = "${themeDir}/catppuccin.theme";
   xdg.configFile."zathura/themes/dark.theme".source = "${themeDir}/gruvbox-dark.theme";
   xdg.configFile."zathura/themes/light.theme".source = "${themeDir}/gruvbox-light.theme";
   xdg.configFile."zathura/change-theme.bash".source = "${themeDir}/change-theme.bash";
 }
-
