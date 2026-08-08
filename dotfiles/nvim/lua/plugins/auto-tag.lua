3b198e01aa98042966f56b24eb4b08e40ce01d7d
dont-wait <nguyentansang15042005@gmail.com>
2026-08-08 13:30:06 +0700

    auto rename tag

--- dotfiles/nvim/lua/plugins/auto-tag.lua
+++ dotfiles/nvim/lua/plugins/auto-tag.lua
@@ -1,20 +1,13 @@
 return {
 	"windwp/nvim-ts-autotag",
 	opts = {
-		-- Defaults
-		enable_close = true, -- Auto close tags
-		enable_rename = true, -- Auto rename pairs of tags
-		enable_close_on_slash = false, -- Auto close on trailing </
-	},
-	-- Also override individual filetype configs, these take priority.
-	-- Empty by default, useful if one of the "opts" global settings
-	-- doesn't work well in a specific filetype
-	config = function()
-		require("nvim-ts-autotag").setup()
+		enable_close = true,
+		enable_rename = true,
+		enable_close_on_slash = false,
 		per_filetype = {
-			["html"] = {
+			html = {
 				enable_close = false,
 			},
-		}
-	end,
+		},
+	},
 }
